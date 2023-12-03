14c146199191afd462ee85e2688cd956fd7725a1
fox <me@iamdavidfox.com>
2023-12-03 01:09:40 -0500

    day 1 part 1

--- Day1.cs
+++ Day1.cs
@@ -1,7 +1,29 @@
+using System.Text.RegularExpressions;
+
 public static class Day1
 {
     public static void Run()
     {
-        Console.WriteLine("yo");
+        var lines = File.ReadAllLines("./day1in.txt");
+        int total = 0;
+        foreach(var line in lines)
+        {
+            // remove all letters from line
+            var digits = Regex.Replace(line, "[^0-9]", "").ToArray();
+            if (digits.Length == 1)
+            {
+                // parse the char to int
+                int num = int.Parse(digits[0].ToString());
+                total += num + (num*10);
+            }
+            else
+            {
+                char firstNum = digits[0];
+                char lastNum = digits[^1];
+                total += int.Parse($"{firstNum}{lastNum}");
+            }
+        }
+
+        Console.WriteLine($"Total: {total}");
     }
 }
